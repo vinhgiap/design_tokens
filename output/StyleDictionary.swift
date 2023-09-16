aa1b9970448a0e4cbe6af50e368bfd1d435f0ee6
vinhgiap <vinhgiap@users.noreply.github.com>
2023-09-16 04:08:49 +0000

    Generate new output after design tokens update [automated]

--- output/StyleDictionary.swift
+++ output/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 16 Sep 2023 03:27:49 GMT
+// Generated on Sat, 16 Sep 2023 04:08:46 GMT
 
 
 import SwiftUI
@@ -24,6 +24,9 @@ public class StyleDictionaryClass {
     public static let globalColorShadowBlue = UIColor(argb: 0x0C6DFF80)
     public static let globalColorShadowGray = UIColor(argb: 0x00000040)
     public static let globalColorStroke = UIColor(rgb: 0xD1D1D1)
+    public static let globalCornerRadiusLg = CGFloat(30)
+    public static let globalCornerRadiusMd = CGFloat(24)
+    public static let globalCornerRadiusSm = CGFloat(10)
     public static let globalFontSizeLg = 18
     public static let globalFontSizeMd = 16
     public static let globalFontSizeSm = 14
@@ -33,20 +36,17 @@ public class StyleDictionaryClass {
         fontWeight: UIFont.Weight.bold,
         fontSize: 16
       )
-    public static let globalLg = CGFloat(30)
-    public static let globalMd = CGFloat(24)
     public static let globalRegular = UIFont.Weight.regular
     public static let globalShadowBlue = TokenShadow(
       offset: CGSize(width: 0, height: 4),
       blur: 16,
-      color: UIColor(argb: 0x0C6DFF80)
+      color: UIColor(rgb: 0x0C6DFF)
     )
     public static let globalShadowGray = TokenShadow(
       offset: CGSize(width: 0, height: 4),
       blur: 16,
-      color: UIColor(argb: 0x00000040)
+      color: UIColor(rgb: 0x000000)
     )
-    public static let globalSm = CGFloat(10)
     public static let globalStroke = TokenBorder(
       borderColor: UIColor(rgb: 0xD1D1D1),
       borderWidth: 1
