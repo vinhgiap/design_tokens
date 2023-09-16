5ca8671927909a54d89f182f326398f1d8b205ac
vinhgiap <vinhgiap@users.noreply.github.com>
2023-09-16 03:27:49 +0000

    Generate new output after design tokens update [automated]

--- output/StyleDictionary.swift
+++ output/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Sat, 16 Sep 2023 03:18:33 GMT
+// Generated on Sat, 16 Sep 2023 03:27:49 GMT
 
 
 import SwiftUI
@@ -29,31 +29,31 @@ public class StyleDictionaryClass {
     public static let globalFontSizeSm = 14
     public static let globalInter = "Inter"
     public static let globalLabel = TokenTypo(
-        fontFamily: Inter,
-        fontSize: 16,
-        fontWeight: Bold
+        fontFamily: "Inter",
+        fontWeight: UIFont.Weight.bold,
+        fontSize: 16
       )
     public static let globalLg = CGFloat(30)
     public static let globalMd = CGFloat(24)
     public static let globalRegular = UIFont.Weight.regular
     public static let globalShadowBlue = TokenShadow(
       offset: CGSize(width: 0, height: 4),
-      color: UIColor(argb: 0x0C6DFF80),
-      blur: 16
+      blur: 16,
+      color: UIColor(argb: 0x0C6DFF80)
     )
     public static let globalShadowGray = TokenShadow(
       offset: CGSize(width: 0, height: 4),
-      color: UIColor(argb: 0x00000040),
-      blur: 16
+      blur: 16,
+      color: UIColor(argb: 0x00000040)
     )
     public static let globalSm = CGFloat(10)
     public static let globalStroke = TokenBorder(
-      borderWidth: 2,
-      borderColor: UIColor(rgb: 0xD1D1D1)
+      borderColor: UIColor(rgb: 0xD1D1D1),
+      borderWidth: 1
     )
     public static let globalTextfield = TokenTypo(
-        fontFamily: Inter,
-        fontSize: 18,
-        fontWeight: Regular
+        fontFamily: "Inter",
+        fontWeight: UIFont.Weight.regular,
+        fontSize: 18
       )
 }
