362d07d11e34454b22178ec021e9c9b423b02f6d
vinhgiap <vinhgiap@users.noreply.github.com>
2023-09-17 20:08:23 +0000

    Generate new output after design tokens update [automated]

--- output/StyleDictionary.swift
+++ output/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Sun, 17 Sep 2023 20:04:22 GMT
+// Generated on Sun, 17 Sep 2023 20:08:23 GMT
 
 
 import SwiftUI
@@ -51,7 +51,7 @@ public class StyleDictionaryClass {
     )
     public static let globalStroke = TokenBorder(
       borderColor: UIColor(rgb: 0xD1D1D1),
-      borderWidth: 0.5
+      borderWidth: 1
     )
     public static let globalTextfield = TokenTypo(
         fontFamily: "Inter-Regular",
