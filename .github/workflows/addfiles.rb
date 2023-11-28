1e09cd3061cacae8183bdd264ee62d2a4d8ec088
CPE07.vinhgv <CPE07.vinhgv@techcombank.com.vn>
2023-11-28 12:19:43 +0700

    push a file

--- .github/workflows/addfiles.rb
+++ .github/workflows/addfiles.rb
@@ -6,15 +6,17 @@ target = project.targets.first
 isExitsfile = false
 files = target.source_build_phase.files.to_a.map do |pbx_build_file|
     pbx_build_file.file_ref.real_path.to_s.include? "StyleDictionary.swift"
-    end
+
+end.select do |isExits|
+    puts isExits
 end
 
-# #find the group on which you want to add the file
-# group = project.main_group["FigmaTokens"]["Components"]
-# #get the file reference for the file to add
-# file = group.new_file("StyleDictionary.swift")
-# #add the file reference to the projects first target
-# main_target = project.targets.first
-# main_target.add_file_references([file])
-# #finally, save the project
-# project.save
+if files.include?(true) 
+#find the group on which you want to add the file
+group = project.main_group["FigmaTokens"]["Components"]
+#get the file reference for the file to add
+file = group.new_file("StyleDictionary.swift")
+#add the file reference to the projects first target
+target.add_file_references([file])
+project.save
+end
