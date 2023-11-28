4eaa53d2af88b20306de96752c31309cbafd5cb5
vinhgiap <vinhgiap@users.noreply.github.com>
2023-11-28 05:34:52 +0000

    Generate new output after design tokens update [automated]

--- output/StyleDictionary.swift
+++ output/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 28 Nov 2023 05:31:59 GMT
+// Generated on Tue, 28 Nov 2023 05:34:51 GMT
 
 
 import SwiftUI
@@ -20,7 +20,7 @@ public class StyleDictionaryClass {
     public static let globalColorLabelPrimary = UIColor(rgb: 0x2F2F2F)
     public static let globalColorLabelSecondary = UIColor(rgb: 0x2F2F2F)
     public static let globalColorLabelSubtitle = UIColor(rgb: 0x2F2F2F)
-    public static let globalColorPrimary = UIColor(rgb: 0x0C6D00)
+    public static let globalColorPrimary = UIColor(rgb: 0x0C6DFF)
     public static let globalColorShadowBlue = UIColor(rgb: 0x0C6DFF)
     public static let globalColorShadowGray = UIColor(rgb: 0x000000)
     public static let globalColorStroke = UIColor(rgb: 0xD1D1D1)
