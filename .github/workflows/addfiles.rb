912d92a8e4c94ce697515d622e0053a9b5acee30
CPE07.vinhgv <CPE07.vinhgv@techcombank.com.vn>
2023-11-28 12:11:43 +0700

    fix size

--- .github/workflows/addfiles.rb
+++ .github/workflows/addfiles.rb
@@ -3,11 +3,10 @@ require 'xcodeproj'
 project_path = 'FigmaTokens/FigmaTokens.xcodeproj'
 project = Xcodeproj::Project.open(project_path)
 target = project.targets.first
+isExitsfile = false
 files = target.source_build_phase.files.to_a.map do |pbx_build_file|
-    pbx_build_file.file_ref.real_path.to_s
-end.select do |path|
-    path.end_with?(".m", ".mm", ".swift")
-  
+    pbx_build_file.file_ref.real_path.to_s.include? "StyleDictionary.swift"
+    
 end.select do |path|
     puts path
 end
