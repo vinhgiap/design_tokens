b343334bc66efcd2efba7a603d542f898494afcd
CPE07.vinhgv <CPE07.vinhgv@techcombank.com.vn>
2023-11-28 11:47:36 +0700

    check all files

--- .github/workflows/addfiles.rb
+++ .github/workflows/addfiles.rb
@@ -2,12 +2,17 @@ require 'xcodeproj'
 
 project_path = 'FigmaTokens/FigmaTokens.xcodeproj'
 project = Xcodeproj::Project.open(project_path)
-#find the group on which you want to add the file
-group = project.main_group["FigmaTokens"]["Components"]
-#get the file reference for the file to add
-file = group.new_file("StyleDictionary.swift")
-#add the file reference to the projects first target
-main_target = project.targets.first
-main_target.add_file_references([file])
-#finally, save the project
-project.save
+target = project.targets.first
+files = target.source_build_phase.files.to_a.map do |pbx_build_file|
+    pbx_build_file.file_ref.real_path.to_s
+puts files
+
+# #find the group on which you want to add the file
+# group = project.main_group["FigmaTokens"]["Components"]
+# #get the file reference for the file to add
+# file = group.new_file("StyleDictionary.swift")
+# #add the file reference to the projects first target
+# main_target = project.targets.first
+# main_target.add_file_references([file])
+# #finally, save the project
+# project.save
